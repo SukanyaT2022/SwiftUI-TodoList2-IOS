437e516996ec305868c19711f6522b4fd94b0b52
SukanyaT2022 <sukanyat2022a@gmail.com>
2025-02-02 18:50:18 -0500

    first

---
+++ Feb2TableViewCellPractice/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  Feb2TableViewCellPractice
+//
+//  Created by Tiparpron Sukanya on 2/2/25.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var studentImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+
+}
--- Feb2TableViewCellPractice/ViewController.swift
+++ Feb2TableViewCellPractice/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
 
+    @IBOutlet weak var firstScreenTableView: UITableView!
+    
+//    struct Student{
+//        let studentName: String
+//            let studentImage: String
+//    }
+//    let data: [Student] = [
+//        Student(studentName:"dido", studentImage:"s1"),
+//       Student(studentName:"kitty", studentImage:"s2"),
+//        Student(studentName:"mummy", studentImage:"s3"),
+//        Student(studentName:"kara", studentImage:"s4"),
+//        Student(studentName:"paepae", studentImage:"s5")
+//        ]
+    
+    let studentName = ["dido", "kitty", "mummy", "kara", "paepae"]
+    let studentImage = ["s1", "s2", "s3", "s4", "s5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        firstScreenTableView.delegate = self
+        firstScreenTableView.dataSource = self
     }
 
 
 }
-
+extension ViewController{
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        <#code#>
+//    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return studentName.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        150
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //deque means insert or remove
+     let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath) as!
+        CustomTableViewCell
+        cell.nameLabel.text = studentName[indexPath.row]
+        cell.studentImageView.image = UIImage(named: studentImage[indexPath.row])
+        return cell
+    }
+}
